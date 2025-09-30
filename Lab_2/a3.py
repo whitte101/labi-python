4eb90e3a67656cf20effeeeb2b9dd4049072e488
Белко Никита <Belko_ND1_25@student.grsu.by>
2025-09-30 08:50:59 +0300

    А часть

---
+++ Lab_2/a3.py
@@ -0,0 +1,4 @@
+bytes_size = 1921000
+megabytes = bytes_size / 1024 / 1024
+
+print(f"Мегабайт: {megabytes:.2f} МБ") 
---
+++ Lab_2/a4.py
@@ -0,0 +1,7 @@
+import random
+A = random.randint(1, 1000)
+B = random.randint(1, 1000)
+if A > B:
+    print(A)
+else:
+    print(B) 
