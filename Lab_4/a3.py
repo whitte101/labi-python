0468f42cc11707b43e016e3e87f20459b2dd5169
whitte101 <Belko_ND1_25@student.grsu.by>
2025-11-18 10:18:20 +0300

    только господ мне поможет

--- Lab_4/a3.py
+++ Lab_4/a3.py
@@ -1,16 +1,50 @@
-def main():
-    packets = input("введите 0 и/или 1:")
-
+def analyze_packet_loss():
+    packets = input("введите последовательность пакетов (0 и 1): ")
+    
     if len(packets) < 5:
-        print("минимальное количество пакетов 5!!!")
+        print("длина строки должна быть не меньше 5 символов!")
         return
     
-    if not all(char in "01" for char in packets):
-        print('допустимы только 0 и 1!!!')
+    if not all(char in '01' for char in packets):
+        print("допустимые символы только: '0' и '1'!")
         return
     
-    vse_pakets = len(packets)
+    total_packets = len(packets)
+    lost_packets = packets.count('0')
+    
+    max_loss_streak = 0
+    current_streak = 0
+    
+    for packet in packets:
+        if packet == '0':
+            current_streak += 1
+            if current_streak > max_loss_streak:
+                max_loss_streak = current_streak
+        else:
+            current_streak = 0
+    
+    loss_percentage = (lost_packets / total_packets) * 100
+    
+    if loss_percentage <= 1:
+        quality = "Отличное качество"
+    elif loss_percentage <= 5:
+        quality = "Хорошее качество"
+    elif loss_percentage <= 10:
+        quality = "Удовлетворительное качество"
+    elif loss_percentage <= 20:
+        quality = "Плохое качество"
+    else:
+        quality = "Критическое состояние сети"
+    
+    print("\n" + "="*50)
+    print("РЕЗУЛЬТАТЫ АНАЛИЗА ПОСЛЕДОВАТЕЛЬНОСТИ ПАКЕТОВ")
+    print("="*50)
+    print(f"Общее количество пакетов: {total_packets}")
+    print(f"Количество потерянных пакетов: {lost_packets}")
+    print(f"Длина самой длинной последовательности потерь: {max_loss_streak}")
+    print(f"Процент потерь: {loss_percentage:.2f}%")
+    print(f"Оценка качества связи: {quality}")
+    print("="*50)
 
-     
-              
-              
+if __name__ == "__main__":
+    analyze_packet_loss()
